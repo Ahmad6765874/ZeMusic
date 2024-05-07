5ec4592a0ffa89fc3298642bd43525aba5a6a386
Ahmad6765874 <150198512+Ahmad6765874@users.noreply.github.com>
2024-05-07 18:39:06 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,26 +5,27 @@ from dotenv import load_dotenv
 from pyrogram import filters
 
 load_dotenv()
-
+ 
 # Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID","25281175"))
-API_HASH = getenv("API_HASH","6d99cb2b60a2c519fc1f99bd19565730")
+API_ID = int(getenv("API_ID","20036317"))
+API_HASH = getenv("API_HASH","986cb4ba434870a62fe96da3b5f6d411")
 
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_NAME = getenv("BOT_NAME","")
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = getenv("MONGO_DB_URI",None)
+MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://abood:king@cluster0.rbp1cqz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 480))
 
-# Chat id of a group for logging bot's activities
+# Chat id of a group for logging bot s activities
 LOGGER_ID = int(getenv("LOGGER_ID"))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6189288231)
+OWNER_ID = int(getenv("OWNER_ID", 5145609515))
 
-## Fill these variables if you're deploying on heroku.
+## Fill these variables if you re deploying on heroku.
 # Your heroku app name
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
@@ -32,14 +33,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/modymesourceze/ZeMusic",
+    "https://github.com/yousef1yt/AMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/O_U_Q1")
+CHANNEL_NAME = getenv("CHANNEL_NAME", "𝚂𝚄𝙾𝚁𝙲𝙴 big Sam")
+CHANNEL_LINK = getenv("CHANNEL_LINK", "https://t.me/O_U_Q1")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/O_U_Q1")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
@@ -51,7 +53,7 @@ SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
 
 
-# Maximum limit for fetching playlist's track from youtube, spotify, apple links.
+# Maximum limit for fetching playlist s track from youtube, spotify, apple links.
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
 
 
@@ -77,18 +79,18 @@ autoclean = []
 confirmer = {}
 
 
-START_IMG_URL = "https://telegra.ph/file/d64e882e61268d454c6c0.jpg"
-PING_IMG_URL = "https://telegra.ph/file/d64e882e61268d454c6c0.jpg"
-PLAYLIST_IMG_URL = "https://telegra.ph/file/d64e882e61268d454c6c0.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/d64e882e61268d454c6c0.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/d64e882e61268d454c6c0.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/d64e882e61268d454c6c0.jpg"
-STREAM_IMG_URL = "https://telegra.ph/file/d64e882e61268d454c6c0.jpg"
-SOUNCLOUD_IMG_URL = "https://telegra.ph/file/d64e882e61268d454c6c0.jpg"
-YOUTUBE_IMG_URL = "https://telegra.ph/file/d64e882e61268d454c6c0.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/d64e882e61268d454c6c0.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/d64e882e61268d454c6c0.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/d64e882e61268d454c6c0.jpg"
+START_IMG_URL = getenv("START_IMG_URL")
+PING_IMG_URL = "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
+PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
+STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
+TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
+SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
+YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
 
 
 def time_to_seconds(time):
@@ -99,10 +101,10 @@ def time_to_seconds(time):
 DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
 
 
-if SUPPORT_CHANNEL:
-    if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
+if CHANNEL_LINK:
+    if not re.match("(?:http|https)://", CHANNEL_LINK):
         raise SystemExit(
-            "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
+            "[ERROR] - Your CHANNEL_LINK url is wrong. Please ensure that it starts with https://"
         )
 
 if SUPPORT_CHAT:
